d86100696e284542c6b213afa73df27f2a990bd0
mromdhani <mromdhani7@gmail.com>
2017-05-09 22:32:20 +0100

    encore et encore

--- Projet-8-NodeJS-ExpressJS-Scaffolded/routes/employes.js
+++ Projet-8-NodeJS-ExpressJS-Scaffolded/routes/employes.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 // some comment
 // encore un commentaire
+// encore et encore
 var employes =[
                 { nom: "Fahmi BEZZAOUIA", poste: "Formateur permanent", age: 40 },
                 { nom: "Sami AYADI", poste: "Formateur permanent", age: 40 },
                 { nom: "Brahim OUESLATI", poste: "Formateur permanent", age: 30 },
                 { nom: "Hanen OUESLATI", poste: "Formateur permanent", age: 18 },
-   { nom: "Mohamed ROMDHANI", poste: "Formateur permanent", age: 45 },
+                { nom: "Mohamed ROMDHANI", poste: "Formateur permanent", age: 45 },
             ];
 
 /* Retourner la liste des employés en JSON */
